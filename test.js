48afd47c39bcacde39710a75af6e598fdb7d9744
Sindre Sorhus <sindresorhus@gmail.com>
2015-09-26 15:11:54 +0700

    ES2015ify tests

--- test.js
+++ test.js
@@ -1,43 +1,41 @@
-'use strict';
+import test from 'ava';
+import chalk from 'chalk';
+import stripAnsi from 'strip-ansi';
+import util from 'util';
+import fn from './';
 
-var test = require('ava');
-var chalk = require('chalk');
-var stripAnsi = require('strip-ansi');
-var util = require('util');
-var fn = require('./');
+const fixture = chalk.red('the ') + chalk.green('quick ') + chalk.blue('brown ') + chalk.cyan('fox ') + chalk.yellow('jumped ');
+const stripped = stripAnsi(fixture);
 
-var fixture = chalk.red('the ') + chalk.green('quick ') + chalk.blue('brown ') + chalk.cyan('fox ') + chalk.yellow('jumped ');
-var stripped = stripAnsi(fixture);
+function gen(str) {
+	const rand1 = ['rock', 'paper', 'scissors']['012'.charAt(Math.floor(Math.random() * 3))];
+	const rand2 = ['blue', 'green', 'yellow', 'red']['0123'.charAt(Math.floor(Math.random() * 4))];
+	return `${str}:${chalk[rand2](rand1)} `;
+}
 
-test(function (t) {
+test(t => {
 	// The slice should behave exactly
 	// as a regular JS slice behaves.
-	for (var i = 0; i < 20; ++i) {
-		for (var j = 19; j > i; --j) {
-			var nativeSlice = stripped.slice(i, j);
-			var ansiSlice = fn(fixture, i, j);
-			t.assert(nativeSlice === stripAnsi(ansiSlice));
+	for (let i = 0; i < 20; ++i) {
+		for (let j = 19; j > i; --j) {
+			const nativeSlice = stripped.slice(i, j);
+			const ansiSlice = fn(fixture, i, j);
+			t.is(nativeSlice, stripAnsi(ansiSlice));
 		}
 	}
 
-	var a = util.inspect('\u001b[31m\u001b[31m\u001b[31m\u001b[31m\u001b[31mthe \u001b[39m\u001b[32mquick \u001b[39m\u001b[34m\u001b[39m');
-	var b = util.inspect('\u001b[32m\u001b[39m\u001b[39m\u001b[39m\u001b[39m\u001b[39m\u001b[34m\u001b[34m\u001b[34m\u001b[34m\u001b[34mbrown \u001b[39m\u001b[36mfox \u001b[39m\u001b[33m\u001b[39m');
-	var c = util.inspect('\u001b[31m \u001b[39m\u001b[32mquick \u001b[39m\u001b[34mbrown \u001b[39m\u001b[36mfox \u001b[39m\u001b[33m\u001b[39m');
+	const a = util.inspect('\u001b[31m\u001b[31m\u001b[31m\u001b[31m\u001b[31mthe \u001b[39m\u001b[32mquick \u001b[39m\u001b[34m\u001b[39m');
+	const b = util.inspect('\u001b[32m\u001b[39m\u001b[39m\u001b[39m\u001b[39m\u001b[39m\u001b[34m\u001b[34m\u001b[34m\u001b[34m\u001b[34mbrown \u001b[39m\u001b[36mfox \u001b[39m\u001b[33m\u001b[39m');
+	const c = util.inspect('\u001b[31m \u001b[39m\u001b[32mquick \u001b[39m\u001b[34mbrown \u001b[39m\u001b[36mfox \u001b[39m\u001b[33m\u001b[39m');
 
-	t.assert(util.inspect(fn(fixture, 0, 10)) === a);
-	t.assert(util.inspect(fn(fixture, 10, 20)) === b);
-	t.assert(util.inspect(fn(fixture, 3, 20)) === c);
+	t.is(util.inspect(fn(fixture, 0, 10)), a);
+	t.is(util.inspect(fn(fixture, 10, 20)), b);
+	t.is(util.inspect(fn(fixture, 3, 20)), c);
 
-	function gen(str) {
-		var rand1 = ['rock', 'paper', 'scissors']['012'.charAt(Math.floor(Math.random() * 3))];
-		var rand2 = ['blue', 'green', 'yellow', 'red']['0123'.charAt(Math.floor(Math.random() * 4))];
-		return str + ':' + chalk[rand2](rand1) + ' ';
-	}
-
-	var str = gen(1) + gen(2) + gen(3) + gen(4) + gen(5) + gen(6) + gen(7) + gen(8) + gen(9) + gen(10) + gen(11) + gen(12) + gen(13) + gen(14) + gen(15) + gen(1) + gen(2) + gen(3) + gen(4) + gen(5) + gen(6) + gen(7) + gen(8) + gen(9) + gen(10) + gen(11) + gen(12) + gen(13) + gen(14) + gen(15);
-	var native = stripAnsi(str).slice(0, 55);
-	var ansi = stripAnsi(fn(str, 0, 55));
-	t.assert(native === ansi);
+	const str = gen(1) + gen(2) + gen(3) + gen(4) + gen(5) + gen(6) + gen(7) + gen(8) + gen(9) + gen(10) + gen(11) + gen(12) + gen(13) + gen(14) + gen(15) + gen(1) + gen(2) + gen(3) + gen(4) + gen(5) + gen(6) + gen(7) + gen(8) + gen(9) + gen(10) + gen(11) + gen(12) + gen(13) + gen(14) + gen(15);
+	const native = stripAnsi(str).slice(0, 55);
+	const ansi = stripAnsi(fn(str, 0, 55));
+	t.is(native, ansi);
 
 	t.end();
 });
