60bb17679d97eb2e9e9e441c3a3fcc806fb907be
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-17 01:44:43 +0700

    Meta tweaks

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ import test from 'ava';
 import chalk from 'chalk';
 import stripAnsi from 'strip-ansi';
 import randomItem from 'random-item';
-import m from '.';
+import sliceAnsi from '.';
 
 const fixture = chalk.red('the ') + chalk.green('quick ') + chalk.blue('brown ') + chalk.cyan('fox ') + chalk.yellow('jumped ');
 const stripped = stripAnsi(fixture);
@@ -19,7 +19,7 @@ test(t => {
 	for (let i = 0; i < 20; i++) {
 		for (let j = 19; j > i; j--) {
 			const nativeSlice = stripped.slice(i, j);
-			const ansiSlice = m(fixture, i, j);
+			const ansiSlice = sliceAnsi(fixture, i, j);
 			t.is(nativeSlice, stripAnsi(ansiSlice));
 		}
 	}
@@ -28,38 +28,38 @@ test(t => {
 	const b = util.inspect('\u001B[32m\u001B[39m\u001B[34mbrown \u001B[39m\u001B[36mfox \u001B[39m\u001B[33m\u001B[39m');
 	const c = util.inspect('\u001B[31m \u001B[39m\u001B[32mquick \u001B[39m\u001B[34mbrown \u001B[39m\u001B[36mfox \u001B[39m\u001B[33m\u001B[39m');
 
-	t.is(util.inspect(m(fixture, 0, 10)), a);
-	t.is(util.inspect(m(fixture, 10, 20)), b);
-	t.is(util.inspect(m(fixture, 3, 20)), c);
+	t.is(util.inspect(sliceAnsi(fixture, 0, 10)), a);
+	t.is(util.inspect(sliceAnsi(fixture, 10, 20)), b);
+	t.is(util.inspect(sliceAnsi(fixture, 3, 20)), c);
 
 	const str = gen(1) + gen(2) + gen(3) + gen(4) + gen(5) + gen(6) + gen(7) + gen(8) + gen(9) + gen(10) + gen(11) + gen(12) + gen(13) + gen(14) + gen(15) + gen(1) + gen(2) + gen(3) + gen(4) + gen(5) + gen(6) + gen(7) + gen(8) + gen(9) + gen(10) + gen(11) + gen(12) + gen(13) + gen(14) + gen(15);
 	const native = stripAnsi(str).slice(0, 55);
-	const ansi = stripAnsi(m(str, 0, 55));
+	const ansi = stripAnsi(sliceAnsi(str, 0, 55));
 	t.is(native, ansi);
 });
 
 test('supports fullwidth characters', t => {
-	t.is(m('안녕하세', 0, 4), '안녕');
+	t.is(sliceAnsi('안녕하세', 0, 4), '안녕');
 });
 
 test('supports unicode surrogate pairs', t => {
-	t.is(m('a\uD83C\uDE00BC', 0, 2), 'a\uD83C\uDE00');
+	t.is(sliceAnsi('a\uD83C\uDE00BC', 0, 2), 'a\uD83C\uDE00');
 });
 
 test('doesn\'t add unnecessary escape codes', t => {
-	t.is(m('\u001B[31municorn\u001B[39m', 0, 3), '\u001B[31muni\u001B[39m');
+	t.is(sliceAnsi('\u001B[31municorn\u001B[39m', 0, 3), '\u001B[31muni\u001B[39m');
 });
 
 test.failing('can slice a normal character before a colored character', t => {
-	t.is(m('a\u001B[31mb\u001B[39m', 0, 1), 'a');
+	t.is(sliceAnsi('a\u001B[31mb\u001B[39m', 0, 1), 'a');
 });
 
 test.failing('can slice a normal character after a colored character', t => {
-	t.is(m('\u001B[31ma\u001B[39mb', 1, 2), 'b');
+	t.is(sliceAnsi('\u001B[31ma\u001B[39mb', 1, 2), 'b');
 });
 
 test('weird null issue', t => {
 	const s = '\u001B[1mautotune.flipCoin("easy as") ? 🎂 : 🍰 \u001B[33m★\u001B[39m\u001B[22m';
-	const result = m(s, 38);
-	t.is(result.search('null'), -1);
+	const result = sliceAnsi(s, 38);
+	t.false(result.includes('null'));
 });
