f4f3cc303c1e1a7905f3dedf855ed2daf89ed1df
Jesús Leganés-Combarro <piranna@gmail.com>
2019-01-25 21:32:14 +0100

    Accept true color escape sequences (#18)
    Fixes #17

--- test.js
+++ test.js
@@ -63,3 +63,7 @@ test('weird null issue', t => {
 	const result = sliceAnsi(s, 38);
 	t.false(result.includes('null'));
 });
+
+test('support true color escape sequences', t => {
+	t.is(sliceAnsi('\u001B[[1m\u001B[[48;2;255;255;255m\u001B[[38;2;255;0;0municorn\u001B[[39m\u001B[[49m\u001B[[22m', 0, 3), '\u001B[1m\u001B[48;2;255;255;25m\u001B[38;2;255;0;0muni\u001B[39m');
+});
