95a3fb3b5c59f9978efa2a59245b3e5c45d1e623
Daniel Spasojevic <dxspasojevic@agiledigital.com.au>
2021-03-22 23:05:01 +1100

    Stringify more test strings to support debugging.

--- test.js
+++ test.js
@@ -90,7 +90,7 @@ test('doesn\'t add extra escapes', t => {
 	const output = `${chalk.black.bgYellow(' RUNS ')}  ${chalk.green('test')}`;
 	t.is(sliceAnsi(output, 0, 7), `${chalk.black.bgYellow(' RUNS ')} `);
 	t.is(sliceAnsi(output, 0, 8), `${chalk.black.bgYellow(' RUNS ')}  `);
-	t.is(sliceAnsi('\u001B[31m' + output, 0, 4), `\u001B[31m${chalk.black.bgYellow(' RUN')}`);
+	t.is(JSON.stringify(sliceAnsi('\u001B[31m' + output, 0, 4)), JSON.stringify(`\u001B[31m${chalk.black.bgYellow(' RUN')}`));
 });
 
 // See https://github.com/chalk/slice-ansi/issues/26
